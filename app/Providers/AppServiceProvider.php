d0cbfb135a4d755fe4dc3547dd7ad878f6449007
ansmalik446 <98810654+ansmalik446@users.noreply.github.com>
2022-03-17 18:24:22 +0500

    camera2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,9 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        //
         
- 
-            \URL::forceScheme('https');
-        
+    \URL::forceScheme('https');
+
     }
 }
--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -224,6 +224,16 @@
         });
         Instascan.Camera.getCameras().then(function (cameras) {
             if (cameras.length > 0) {
+              if(cameras.length >1)
+              {
+                scanner.start(cameras[2]);
+
+              }
+              else
+              {
+                scanner.start(cameras[1]);
+
+              }
                 scanner.start(cameras[0]);
             } else {
                 console.error('No cameras found.');
