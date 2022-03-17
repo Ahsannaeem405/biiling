4372d2fc0ee8d42f126900e350205c7be2c4e166
ansmalik446 <98810654+ansmalik446@users.noreply.github.com>
2022-03-17 18:49:57 +0500

    scannerlistner

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -224,20 +224,17 @@
         });
         Instascan.Camera.getCameras().then(function (cameras) {
             if (cameras.length > 0) {
-              if(cameras.length >1)
-              {
-                scanner.start(cameras[1]);
-
-              }
-              else{
+              
                 scanner.start(cameras[0]);
-
-              }
             } else {
                 console.error('No cameras found.');
             }
         }).catch(function (e) {
             console.error(e);
+        });
+        scanner.addListener('scan',function(c){
+            alert(c);
+            document.getElementById('aadhar').value=c;
         });
       }
 
