5cc24ad291820c7407cdc05d3d59550774c3aab7
ansmalik446 <98810654+ansmalik446@users.noreply.github.com>
2022-03-21 12:26:48 +0500

    Update index.blade.php

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -222,15 +222,13 @@
 
 <script src="https://unpkg.com/html5-qrcode@2.0.9/dist/html5-qrcode.min.js"></script>
 
-
-
 <script>
   function onScanSuccess(decodedText, decodedResult) {
     console.log(`Code scanned = ${decodedText}`, decodedResult);
     alert(decodedText);
 }
 var html5QrcodeScanner = new Html5QrcodeScanner(
-	"qr-reader", { fps: 10, qrbox: 300 });
+	"qr-reader", { fps: 10, qrbox: 250 });
 html5QrcodeScanner.render(onScanSuccess);
 
 </script>
